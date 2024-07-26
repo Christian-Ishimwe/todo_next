29f6537a438afb48c2a2bccb87ffc52e6ecc1c03
christian ISHIMWE <christianinja3@gmail.com>
2024-07-26 10:07:06 +0200

    Fixes the Db

--- actions/todoActions.ts
+++ actions/todoActions.ts
@@ -1,6 +1,7 @@
 import db from "@/db/drizzle";
 import { todo } from "@/db/schema";
 import { asc, eq, not } from "drizzle-orm";
+import { revalidatePath } from "next/cache";
 
 export const addTodo = async (id: string, text: string) => {
   await db.insert(todo).values({
@@ -10,12 +11,13 @@ export const addTodo = async (id: string, text: string) => {
 };
 
 export const getTodo = async () => {
-  const data = await db.select().from(todo).orderBy(asc(todo.id));
+  const data = await db.select().from(todo);
+  revalidatePath("/")
   return data;
 };
 
 export const editTodo = async (id: string, text: string) => {
-  await db.update(todo).set({ text: text }).where(eq(todo.id, id));
+  await db.update(todo).set({ text: text }).where(eq(todo.id, id))
 };
 
 export const toggleTodo = async (id: string) => {
